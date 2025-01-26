00ca20e2a6774dffc10b217665f275d44d07f8fa
Lorenzzz <lorenzosaavedra.c@gmail.com>
2025-01-26 15:20:21 -0300

    diferencing hand tracker and added routes for testing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import CameraComponent from './components/camara/CameraComponent.tsx'
+import Camera from './components/lore/Camera.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />} />
+                <Route path='/camera' element={<CameraComponent />} />
+                <Route path='/lore' element={<Camera />} />
+
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
