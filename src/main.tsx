081977de8b62ed253333aff52593d6c9b236949a
Ivn <lorenzosaavedra.c@gmail.com>
2025-04-13 19:41:34 -0400

    sync with main project

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import CameraComponent from './components/camara/CameraComponent.tsx'
-import Camera from './components/lore/Camera.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='/camera' element={<CameraComponent />} />
-                <Route path='/lore' element={<Camera />} />
-
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
