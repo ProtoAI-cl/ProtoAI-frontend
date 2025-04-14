e859134d3c1df174de4761a278b9d48dc1ef2126
Ivn <lorenzosaavedra.c@gmail.com>
2025-04-13 20:14:31 -0400

    changed camera functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { InputContainer } from "./components/input/InputContainer";
 import { Links } from "./components/links/Links";
 
 function App() {
-  return (
-    <>
-      <h1>PROTO AI</h1>
-      <h2>Prueba las imágenes!</h2>
-      <CardsContainer />
-      <InputContainer />
-      <Links />
-      <CameraComponent/>
-    </>
-  );
+    return (
+        <>
+            <h1>PROTO AI</h1>
+            <h2>Prueba las imágenes!</h2>
+            <CardsContainer />
+            <InputContainer />
+            <Links />
+            <CameraComponent />
+        </>
+    );
 }
 
 export default App;
