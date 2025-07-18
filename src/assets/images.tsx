3a0503e8d8f708aeee50e8b384bdede36c79b51f
R. M. <56444052+Rena006@users.noreply.github.com>
2025-07-17 21:33:32 -0400

    The name of the Furbol Ball is changed to Soccer Ball

--- src/assets/images.tsx
+++ src/assets/images.tsx
@@ -17,7 +17,7 @@ export const testsImages = [
   {
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/%D0%A4%D0%9A_%22%D0%9A%D0%BE%D0%BB%D0%BE%D1%81%22_%28%D0%97%D0%B0%D1%87%D0%B5%D0%BF%D0%B8%D0%BB%D0%BE%D0%B2%D0%BA%D0%B0%2C_%D0%A5%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%29_-_%D0%A4%D0%9A_%22%D0%91%D0%B0%D0%BB%D0%BA%D0%B0%D0%BD%D1%8B%22_%28%D0%97%D0%B0%D1%80%D1%8F%2C_%D0%9E%D0%B4%D0%B5%D1%81%D1%81%D0%BA%D0%B0%D1%8F_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%29_%2818790931100%29.jpg/800px-thumbnail.jpg",
-    title: "pelota de furbol",
+    title: "pelota de fútbol",
   },
   {
     imageUrl: "https://i.imgur.com/R1dk0KM.jpeg",
